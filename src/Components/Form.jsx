8aa751e678bdc9db1a8168ac61bb762ca1cf0b60
Youssef Ab <youssef.abelouass@gmail.com>
2023-11-21 12:07:21 +0100

    added client side validation for the credit card number input
    added a function that checks the length and the validity of the credit card number using luhn's algorithm, the functions gets triggered once the user unfocuses the input and throws an error if the conditions aren't met

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -19,13 +19,50 @@ function Form(props) {
     )
   }
   const validatingTheCardNumber = () => {
-    return props.cardNumberInput.length === 12
+    /*luhn's algorithm to check whether the credit card number is valid or not*/
+    const luhnCheck = (val) => {
+      let checksum = 0 // running checksum total
+      let j = 1 // takes value of 1 or 2
+
+      // Process each digit one by one starting from the last
+      for (let i = val.length - 1; i >= 0; i--) {
+        let calc = 0
+        // Extract the next digit and multiply by 1 or 2 on alternative digits.
+        calc = Number(val.charAt(i)) * j
+
+        // If the result is in two digits add 1 to the checksum total
+        if (calc > 9) {
+          checksum = checksum + 1
+          calc = calc - 10
+        }
+
+        // Add the units element to the checksum total
+        checksum = checksum + calc
+
+        // Switch the value of j
+        if (j == 1) {
+          j = 2
+        } else {
+          j = 1
+        }
+      }
+
+      //Check if it is divisible by 10 or not.
+      return checksum % 10 == 0
+    }
+    if (
+      !(props.cardNumberInput.length === 16) ||
+      !props.cardNumberInput.match(/^[0-9]*$/) ||
+      !luhnCheck(props.cardNumberInput)
+    ) {
+      setIsTheCardNumberValid(false)
+    }
   }
   const validatingTheCardHolder = () => {
     return props.cardHolderInput != undefined
   }
   const validatingTheExpirationDate = () => {
-    /*comparing the current date with the expiration date that the user has inputed, the expiration date shouldn't be in the past */
+    /*comparing the current date with the expiration date that the user was inputed, the expiration date shouldn't be in the past */
     const date = new Date()
     const [month, year] = props.expirationDate.split("/")
     const formattedExpirationDate = new Date(`${year}-${month}`)
